a63f9cd2a7bafe63fbd6575b9d822656dbd16e24
AustinJackS <austinjack404@gmail.com>
2017-06-09 17:49:04 -0400

    Add files via upload

---
+++ assign.js
@@ -0,0 +1,5 @@
+document.getElementById("1").onclick=function() {load('tab')}
+document.getElementById("2").onclick=function() {load('alert')}
+document.getElementById("3").onclick=function() {load('csa')}
+document.getElementById("4").onclick=function() {load('href')}
+document.getElementById("5").onclick=function() {load('keylog')}
